d33767c0248f351176a6d3cfa4f3dc06eb59a932
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2017-01-04 17:44:06 +0200

    [Tools][Airlines] The airline data file Python generator now has got a bottom line working. Still needs some significant work.

--- tools/make_optd_airline_public.py
+++ tools/make_optd_airline_public.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 
 import getopt, sys, re, csv
-import networkx as nx
 from operator import add
 from collections import OrderedDict, Mapping
-from itertools import chain
 
 #
 # Default file-paths for input and output data files
 #
 def_airline_bestknown_filepath = '../opentraveldata/optd_airline_best_known_so_far.csv'
 def_airline_no_longer_valid_filepath = '../opentraveldata/optd_airline_no_longer_valid.csv'
+def_airline_alliance_filepath = '../opentraveldata/optd_airline_alliance_membership.csv'
 def_freq_filepath = '../opentraveldata/ref_airline_nb_of_flights.csv'
 def_airline_filepath = '../opentraveldata/optd_airlines.csv'
-
-#
-_FLAG_FIRST = object()
 
 #
 # Usage
@@ -39,9 +35,12 @@ def usage (script_name):
     print ("  -n, --no-longer-valid-airline <OPTD no longer valid airlines file-path> :")
     print ("\tInput data file of no longer valid airline details")
     print ("\tDefault: '" + def_airline_no_longer_valid_filepath + "'")
+    print ("  -l, --alliance <OPTD airline alliance file-path> :")
+    print ("\tInput data file of airline alliance details")
+    print ("\tDefault: '" + def_airline_alliance_filepath + "'")
     print ("  -f, --freq <Flight frequency file-path> :")
     print ("\tInput data file of flight frequency values")
-    print ("\tDefault: '" + def_freq_out_filepath + "'")
+    print ("\tDefault: '" + def_freq_filepath + "'")
     print ("  -a, --airline <OPTD airline file-path> :")
     print ("\tOutput data file of airline details")
     print ("\tDefault: '" + def_airline_filepath + "'")
@@ -56,10 +55,10 @@ def handle_opt():
     """
 
     try:
-        opts, args = getopt.getopt (sys.argv[1:], "hv:p:a:b:n:f:",
+        opts, args = getopt.getopt (sys.argv[1:], "hv:p:a:b:n:l:f:",
                                     ["help", "verbose", "airline-por", 
                                      "airline", "best-known-airline",
-                                     "no-longer-valid-airline",
+                                     "no-longer-valid-airline", "alliance",
                                      "freq"])
 
     except (getopt.GetoptError, err):
@@ -73,6 +72,7 @@ def handle_opt():
     verboseFlag = False
     airline_bestknown_filepath = def_airline_bestknown_filepath
     airline_no_longer_valid_filepath = def_airline_no_longer_valid_filepath
+    airline_alliance_filepath = def_airline_alliance_filepath
     freq_filepath = def_freq_filepath
     airline_filepath = def_airline_filepath
 
@@ -87,6 +87,8 @@ def handle_opt():
             airline_bestknown_filepath = a
         elif o in ("-n", "--no-longer-valid-airline"):
             airline_no_longer_valid_filepath = a
+        elif o in ("-l", "--alliance"):
+            airline_alliance_filepath = a
         elif o in ("-f", "--freq"):
             freq_filepath = a
         elif o in ("-a", "--airline"):
@@ -99,81 +101,94 @@ def handle_opt():
     print ("Input data file of no longer valid airline details: '" + airline_no_longer_valid_filepath + "'")
     print ("Input data file of flight frequency values: '" + freq_filepath + "'")
     print ("Output data file of airline details: '" + airline_filepath + "'")
-    return (verboseFlag, airline_bestknown_filepath, airline_no_longer_valid_filepath, freq_filepath, airline_filepath)
+    return (verboseFlag, airline_bestknown_filepath,
+            airline_no_longer_valid_filepath, airline_alliance_filepath,
+            freq_filepath, airline_filepath)
 
 #
-# Store the POR details
+# Extract the details of airlines from the OpenTravelData CSV files
 #
-def storePOR (por_all_dict, por_code, por_iata_code, por_type, por_geoid, por_cty_code_list):
-    # Initialize the per-POR dictionary when needed
-    if (not por_code in por_all_dict):
-        por_all_dict[por_code] = dict()
-
-    por_pk = buildPK (por_iata_code, por_type, por_geoid)
-    por_dict = {'iata_code': por_iata_code, 'por_code': por_code,
-                'location_type': por_type,
-                'geoname_id': por_geoid, 'city_code_list': por_cty_code_list}
-    por_all_dict[por_code][por_pk] = por_dict
-    por_all_dict[por_code]['notified'] = False
-
-    return
-
+# pk^env_id^validity_from^validity_to^3char_code^2char_code^num_code^name^name2^alliance_code^alliance_status^type^wiki_link^alt_names^bases^key^version^parent_pk_list
 #
-# Extract the best known details of airlines from the OpenTravelData CSV file
-#
-# pk^env_id^validity_from^validity_to^3char_code^2char_code^num_code^name^name2^alliance_code^alliance_status^type^wiki_link^flt_freq^alt_names^bases^key^version^parent_pk_list
-#
-def extractBksfAirline (airline_all_dict, airline_bestknown_filepath,
-                        verboseFlag):
+def extractAirlineDetails (airline_all_dict, airline_filepath, verboseFlag):
     """
     Derive a dictionary of all the airlines referenced within the OpenTravelData
     project as 'best known so far' (bksf)
     """
-    with open (airline_bestknown_filepath, newline='') as csvfile:
+    with open (airline_filepath, newline='') as csvfile:
         file_reader = csv.DictReader (csvfile, delimiter='^')
         for row in file_reader:
-            #pk = row['pk']
+            pk = row['pk']
+            env_id = row['env_id']
+            validity_from = row['validity_from']
+            validity_to = row['validity_to']
             iata_code = row['2char_code']
             icao_code = row['3char_code']
-            airline_code = iata_code
-            if icao_code == "": icao_code = "ZZZ"
-            if airline_code == "": airline_code = icao_code
-            env_id = row['env_id']
-            airline_name = row['name']
+            num_code = row['num_code']
+            air_name_utf8 = row['name']
+            air_name_asc = row['name2']
+            alliance_code = ""
+            alliance_status = ""
+            air_type = row['type']
+            wiki_link = row['wiki_link']
+            alt_names = row['alt_names']
+            bases = row['bases']
+            air_key = row['key']
+            air_version = row['version']
+            parent_pk_list = row['parent_pk_list']
 
-            # Register the airline, if active and not already registered
-            if not (airline_code in airline_all_dict) and env_id == '':
-                airline_all_dict[airline_code] = dict()
-
-            if env_id == '':
-                airline_all_dict[airline_code] = {'iata_code': iata_code,
-                                                  'icao_code': icao_code,
-                                                  'nb_seats': 0,
-                                                  'flt_freq': 0,
-                                                  'notified': False}
+            # Register the airline, if not already registered
+            air_code = iata_code + "^" + icao_code
+            if not (air_code in airline_all_dict):
+                airline_all_dict[air_code] = {'pk': pk, 'key': air_key,
+                                              'version': air_version,
+                                              'env_id': env_id,
+                                              'validity_from': validity_from,
+                                              'validity_to': validity_to,
+                                              '2char_code': iata_code,
+                                              '3char_code': icao_code,
+                                              'num_code': num_code,
+                                              'type': air_type,
+                                              'name': air_name_utf8,
+                                              'name2': air_name_asc,
+                                              'alliance_code': alliance_code,
+                                              'alliance_status': alliance_status,
+                                              'wiki_link': wiki_link,
+                                              'alt_names': alt_names,
+                                              'bases': bases,
+                                              'parent_pk_list': parent_pk_list,
+                                              'successor_pk_list': "",
+                                              'flt_freq': 0}
 
     return
 
 #
-# Extract the details of both valid and no longer valid airlines,
-# from the two corresponding OpenTravelData CSV files
+# Extract the route frequencies from the OpenTravelData CSV file
 #
-def extractAirlines (airline_all_dict, airline_bestknown_filepath,
-                     airline_no_longer_valid_filepath, verboseFlag):
+# iata_code^icao_code^nb_seats^flight_freq
+#
+def extractFrequencies (airline_all_dict, freq_filepath, verboseFlag):
     """
-    Derive a dictionary of all the airlines referenced within the OpenTravelData
-    project as 'best known so far' (bksf)
+    Complete the airline details with the flight route frequencies
     """
-    
-    return
+    with open (freq_filepath, newline='') as csvfile:
+        file_reader = csv.DictReader (csvfile, delimiter='^')
+        for row in file_reader:
+            iata_code = row['iata_code']
+            icao_code = row['icao_code']
+            if (icao_code == "ZZZ"): icao_code = ""
+            flt_freq = int(row['flight_freq'])
 
-#
-# Add the flight frequencies
-#
-def addFrequencies (airline_all_dict, freq_filepath, verboseFlag):
-    """
-    Add the flight frequencies
-    """
+            air_code = iata_code + "^" + icao_code
+            if not (air_code in airline_all_dict):
+                print ("The airline '" + iata_code + "/" + icao_code
+                       + "' cannot be found in the OPTD airline data files ('"
+                       + def_airline_bestknown_filepath + "' and '"
+                       + def_airline_no_longer_valid_filepath + "')")
+                raise KeyError
+            else:
+                airline_all_dict[air_code]['flt_freq'] = flt_freq
+
     return
 
 #
@@ -198,43 +213,13 @@ def sortAirlineDict (unsorted_dict):
             return ""
     
     sorted_dict = OrderedDict (sorted (unsorted_dict.items(),
-                                       key = sort_function, reverse=True))
+                                       key = sort_function, reverse=False))
 
     # DEBUG
     # from pprint import pprint as pp
     # pp (sorted_dict)
     
     return sorted_dict
-
-#
-# Filter in only the fields to be dumped into the CSV file
-# ['pk', 'env_id', 'validity_from', 'validity_to', '3char_code',
-# '2char_code', 'num_code', 'name', 'name2', 'alliance_code',
-# 'alliance_status', 'type', 'wiki_link', 'flt_freq',
-# 'alt_names', 'bases', 'key', 'version', 'parent_pk_list',
-# 'successor_pk_list']
-#
-def filterOutAirlineFields (airline_dict, fieldnames):
-    # Retrieve the IATA code
-    assert (fieldnames[0] == 'iata_code'), "The first field is not 'iata_code'!"
-    iata_code = airline_dict[fieldnames[0]]
-
-    # Retrieve the ICAO code
-    assert (fieldnames[1] == 'icao_code'), "The second field is not 'icao_code'!"
-    icao_code = airline_dict[fieldnames[1]]
-
-    # Retrieve the number of seats
-    assert (fieldnames[2] == 'nb_seats'), "The third field is not 'nb_seats'!"
-    nb_seats = airline_dict[fieldnames[2]]
-
-    # Retrieve the flight frequency
-    assert (fieldnames[3] == 'flight_freq'), "The fourth field is not 'flight_freq'!"
-    flt_freq = airline_dict['flt_freq']
-
-    #
-    airline_dict_fltd = {fieldnames[0]: iata_code, fieldnames[1]: icao_code,
-                         fieldnames[2]: nb_seats, fieldnames[3]: flt_freq}
-    return airline_dict_fltd
 
 #
 # Dump the details of all the airlines into the given file
@@ -265,9 +250,10 @@ def dump_airlines (airline_all_dict, output_filepath, verboseFlag):
 
         # Browse the POR having a PageRank value and dump the details
         for (idx_airline, airline_dict) in airline_all_dict_sorted.items():
-            # Filter out the fields not to be dumped into the CSV file
-            airline_dict_fltd = filterOutAirlineFields (airline_dict, fieldnames)
-            fileWriter.writerow (airline_dict_fltd)
+            try:
+                fileWriter.writerow (airline_dict)
+            except:
+                print ("Faulty row: " + str(airline_dict))
 
     return
 
@@ -280,22 +266,28 @@ def main():
     """
 
     # Parse command options
-    (verboseFlag, airline_bestknown_filepath, airline_no_longer_valid_filepath, freq_filepath, airline_filepath) = handle_opt()
+    (verboseFlag, airline_bestknown_filepath, airline_no_longer_valid_filepath, airline_alliance_filepath, freq_filepath, airline_filepath) = handle_opt()
 
     # Extract the airline details from OpenTravelData (both from the file
     # of best known details and from the file of no longer valid airlines)
     airline_all_dict = dict()
-    extractAirlines (airline_all_dict, airline_bestknown_filepath,
-                     airline_no_longer_valid_filepath, verboseFlag)
+    extractAirlineDetails (airline_all_dict, airline_bestknown_filepath,
+                           verboseFlag)
+    extractAirlineDetails (airline_all_dict, airline_no_longer_valid_filepath,
+                           verboseFlag)
 
     # Add the flight frequencies
-    addFrequencies (airline_all_dict, freq_filepath, verboseFlag)
+    extractFrequencies (airline_all_dict, freq_filepath, verboseFlag)
+
+    # DEBUG
+    # from pprint import pprint as pp
+    # pp (airline_all_dict)
 
     # Derive the successors (eg, "merged into" or "rebranded as")
     calculateSuccessors (airline_all_dict, verboseFlag)
 
     # Dump the airline details into the output file
-    #dump_airlines (airline_all_dict, airline_filepath, verboseFlag)
+    dump_airlines (airline_all_dict, airline_filepath, verboseFlag)
 
 
 #
