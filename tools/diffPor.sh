4944475a8acc2af909085840b27e0dde23a9af3f
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2018-08-03 18:48:43 +0200

    [Tools][POR] Slightly improved the Geonames data extract support utility

--- tools/diffPor.sh
+++ tools/diffPor.sh
@@ -10,9 +10,18 @@ fi
 
 ##
 # Snapshot date
+YEAR=`$DATE_TOOL "+%Y"`
 TODAY_DATE=`$DATE_TOOL "+%Y%m%d"`
 TODAY_DATE_HUMAN=`$DATE_TOOL`
 YEST_DATE=`$DATE_TOOL "+%Y%m%d" --date="yesterday"`
+
+##
+# Archive folder
+ARCH_DIR="archives/${YEAR}"
+if [ ! -d ${ARCH_DIR} ]
+then
+	mkdir -p ${ARCH_DIR}
+fi
 
 #
 echo "On the remote host:"
@@ -21,9 +30,11 @@ echo "bzip2 por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
 echo
 echo "On the local host:"
 echo "scp myuser@remote:~/dev/geo/opentraveldata/tools/{por_iata_${TODAY_DATE}.csv.bz2,por_noiata_${YEST_DATE}_${TODAY_DATE}.csv.bz2} ."
-echo "bunzip2 -k por_iata_${TODAY_DATE}.csv.bz2 && mv por_iata_${TODAY_DATE}.csv dump_from_geonames.csv && mv por_iata_${TODAY_DATE}.csv.bz2 archives/2018/"
+echo "bunzip2 -k por_iata_${TODAY_DATE}.csv.bz2 && mv por_iata_${TODAY_DATE}.csv dump_from_geonames.csv && mv por_iata_${TODAY_DATE}.csv.bz2 ${ARCH_DIR}"
 echo "bunzip2 por_noiata_${YEST_DATE}_${TODAY_DATE}.csv.bz2"
 echo "patch -p0 --dry-run < por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
 echo "patch -p0 < por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
+echo "mv por_noiata_${YEST_DATE}.csv por_noiata_${TODAY_DATE}.csv"
+echo "rm -f por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
 
 
