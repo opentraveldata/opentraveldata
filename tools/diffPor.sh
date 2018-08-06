9f456a52aff22aa831d0b240786fe770b1ce9ba2
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2018-08-06 15:33:15 +0200

    [Tools][POR] The Geonames data import support utility adapts to the local update frequency

--- tools/diffPor.sh
+++ tools/diffPor.sh
@@ -13,7 +13,6 @@ fi
 YEAR=`$DATE_TOOL "+%Y"`
 TODAY_DATE=`$DATE_TOOL "+%Y%m%d"`
 TODAY_DATE_HUMAN=`$DATE_TOOL`
-YEST_DATE=`$DATE_TOOL "+%Y%m%d" --date="yesterday"`
 
 ##
 # Archive folder
@@ -23,18 +22,39 @@ then
 	mkdir -p ${ARCH_DIR}
 fi
 
+##
+# Retrieve the latest file
+POR_FILE_PFX=por_noiata
+SNPSHT_DATE=`ls ${POR_FILE_PFX}_????????.{csv,.csv.bz2} 2> /dev/null`
+if [ "${SNPSHT_DATE}" != "" ]
+then
+	# (Trick to) Extract the latest entry
+	for myfile in ${SNPSHT_DATE}; do echo > /dev/null; done
+	SNPSHT_DATE=`echo ${myfile} | sed -e "s/${POR_FILE_PFX}_\([0-9]\+\)\.csv.*/\1/" | xargs basename`
+else
+	echo
+	echo "[$0:$LINENO] No Geonames-derived POR list CSV dump can be found."
+	echo "Expecting a file named like '${POR_FILE_PFX}_YYYYMMDD.csv'"
+	echo
+	exit -1
+fi
+if [ "${SNPSHT_DATE}" != "" ]
+then
+	SNPSHT_DATE_HUMAN=`$DATE_TOOL -d ${SNPSHT_DATE}`
+else
+	SNPSHT_DATE_HUMAN=`$DATE_TOOL --date='yesterday'`
+fi
+
 #
 echo "On the remote host:"
-echo "time diff -Naur por_noiata_${YEST_DATE}.csv por_noiata_${TODAY_DATE}.csv > por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
-echo "bzip2 por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
+echo "time diff -Naur ${POR_FILE_PFX}_${SNPSHT_DATE}.csv ${POR_FILE_PFX}_${TODAY_DATE}.csv > ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv"
+echo "bzip2 ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv"
 echo
 echo "On the local host:"
-echo "scp myuser@remote:~/dev/geo/opentraveldata/tools/{por_iata_${TODAY_DATE}.csv.bz2,por_noiata_${YEST_DATE}_${TODAY_DATE}.csv.bz2} ."
+echo "scp myuser@remote:~/dev/geo/opentraveldata/tools/{por_iata_${TODAY_DATE}.csv.bz2,${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv.bz2} ."
 echo "bunzip2 -k por_iata_${TODAY_DATE}.csv.bz2 && mv por_iata_${TODAY_DATE}.csv dump_from_geonames.csv && mv por_iata_${TODAY_DATE}.csv.bz2 ${ARCH_DIR}"
-echo "bunzip2 por_noiata_${YEST_DATE}_${TODAY_DATE}.csv.bz2"
-echo "patch -p0 --dry-run < por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
-echo "patch -p0 < por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
-echo "mv por_noiata_${YEST_DATE}.csv por_noiata_${TODAY_DATE}.csv"
-echo "rm -f por_noiata_${YEST_DATE}_${TODAY_DATE}.csv"
+echo "bunzip2 ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv.bz2"
+echo "patch -p0 --dry-run < ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv"
+echo "patch -p0 < ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv && mv ${POR_FILE_PFX}_${SNPSHT_DATE}.csv ${POR_FILE_PFX}_${TODAY_DATE}.csv && rm -f ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv"
 
 
