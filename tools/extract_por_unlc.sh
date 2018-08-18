bb3402361fbd33b163b222cd6fb7357876a52f08
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2018-08-19 00:21:56 +0300

    [POR][Tools] Updated the UN/LOCODE-referenced POR extractor

--- tools/extract_por_unlc.sh
+++ tools/extract_por_unlc.sh
@@ -32,9 +32,9 @@ then
     EXEC_PATH="."
     TMP_DIR="."
 fi
-# If the IATA dump file is in the current directory, then the current
-# directory is certainly intended to be the temporary directory.
-if [ -f ${IATA_TAB_FILENAME} ]
+# If the international org-reference POR dump file is in the current directory,
+# then the current directory is certainly intended to be the temporary directory.
+if [ -f ${INTORG_TAB_FILENAME} ]
 then
     TMP_DIR="."
 fi
@@ -70,8 +70,8 @@ fi
 
 ##
 # Retrieve the latest file
-POR_FILE_PFX=por_iata
-POR_NOIATA_FILE_PFX=por_noiata
+POR_FILE_PFX=por_intorg
+POR_ALL_FILE_PFX=por_all
 SNPSHT_DATE=`ls ${TOOLS_DIR}${POR_FILE_PFX}_????????.csv 2> /dev/null`
 if [ "${SNPSHT_DATE}" != "" ]
 then
@@ -81,15 +81,15 @@ then
 else
         echo
         echo "[$0:$LINENO] No non-IATA POR list CSV dump can be found in the '${TOOLS_DIR}' directory."
-        echo "Expecting a file named like '${TOOLS_DIR}${POR_FILE_PFX}_YYYYMMDD.txt'"
+        echo "Expecting a file named like '${TOOLS_DIR}${POR_FILE_PFX}_YYYYMMDD.csv'"
         echo
         exit -1
 fi
 
 #
 SNPSHT_DATE_HUMAN=`${DATE_TOOL} -d ${SNPSHT_DATE}`
-POR_IATA_FILE="${POR_FILE_PFX}_${SNPSHT_DATE}.csv"
-POR_NOIATA_FILE="${POR_NOIATA_FILE_PFX}_${SNPSHT_DATE}.csv"
+POR_INTORG_FILE="${POR_FILE_PFX}_${SNPSHT_DATE}.csv"
+POR_ALL_FILE="${POR_ALL_FILE_PFX}_${SNPSHT_DATE}.csv"
 TGT_FILE="${DATA_DIR}optd_por_unlc.csv"
 TMP_TGT_FILE=${TGT_FILE}.tmp
 STD_TGT_FILE=${TGT_FILE}.std
@@ -97,18 +97,20 @@ HDR_TGT_FILE=${TGT_FILE}.hdr
 
 # Processing
 PROCESSOR="extract_por_unlc.awk"
-time awk -F'^' -f ${PROCESSOR} ${POR_IATA_FILE} ${POR_NOIATA_FILE} > $TMP_TGT_FILE
-sort -t'^' -k1,1 $TMP_TGT_FILE | uniq > $STD_TGT_FILE
-echo "unlocode^latitude^longitude^geonames_id^feat_class^feat_code" > $HDR_TGT_FILE
-cat $HDR_TGT_FILE $STD_TGT_FILE > $TGT_FILE
+time awk -F'^' -f ${PROCESSOR} ${POR_INTORG_FILE} ${POR_ALL_FILE} \
+	 > ${TMP_TGT_FILE}
+sort -t'^' -k1,1 ${TMP_TGT_FILE} | uniq > ${STD_TGT_FILE}
+echo "unlocode^latitude^longitude^geonames_id^feat_class^feat_code" \
+	 > ${HDR_TGT_FILE}
+cat ${HDR_TGT_FILE} ${STD_TGT_FILE} > ${TGT_FILE}
 
 # Cleaning
-\rm -f $TMP_TGT_FILE $HDR_TGT_FILE $STD_TGT_FILE
+\rm -f ${TMP_TGT_FILE} ${HDR_TGT_FILE} ${STD_TGT_FILE}
 
 # Reporting
 NB_POR=`wc -l ${TGT_FILE}|sed -e 's/^\([^0-9]*\)\([0-9]\+\)\([^0-9]\)*$/\2/g'`
 echo
-echo "The UN/LOCODE POR file ('$TGT_FILE') has been generated from '$POR_FILE'"
-echo "There are $NB_POR records"
+echo "The UN/LOCODE POR file ('${TGT_FILE}') has been generated from '${POR_INTORG_FILE}' and '${POR_ALL_FILE}'"
+echo "There are ${NB_POR} records"
 echo
 
