a98bebb0baed6ded9ddee4d6a258b8720aafc206
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2019-01-04 16:47:53 +0200

    [POR][Tools] Added a small handy hint in the POR update helper

--- tools/diffPor.sh
+++ tools/diffPor.sh
@@ -10,9 +10,9 @@ fi
 
 ##
 # Snapshot date
-YEAR=`$DATE_TOOL "+%Y"`
-TODAY_DATE=`$DATE_TOOL "+%Y%m%d"`
-TODAY_DATE_HUMAN=`$DATE_TOOL`
+YEAR=$(${DATE_TOOL} "+%Y")
+TODAY_DATE=$(${DATE_TOOL} "+%Y%m%d")
+TODAY_DATE_HUMAN=$(${DATE_TOOL})
 
 ##
 # Archive folder
@@ -25,12 +25,12 @@ fi
 ##
 # Retrieve the latest file
 POR_FILE_PFX=por_all
-SNPSHT_DATE=`ls ${POR_FILE_PFX}_????????.{csv,.csv.bz2} 2> /dev/null`
+SNPSHT_DATE=$(ls ${POR_FILE_PFX}_????????.{csv,.csv.bz2} 2> /dev/null)
 if [ "${SNPSHT_DATE}" != "" ]
 then
 	# (Trick to) Extract the latest entry
 	for myfile in ${SNPSHT_DATE}; do echo > /dev/null; done
-	SNPSHT_DATE=`echo ${myfile} | sed -e "s/${POR_FILE_PFX}_\([0-9]\+\)\.csv.*/\1/" | xargs basename`
+	SNPSHT_DATE=$(echo ${myfile} | sed -e "s/${POR_FILE_PFX}_\([0-9]\+\)\.csv.*/\1/" | xargs basename)
 else
 	echo
 	echo "[$0:$LINENO] No Geonames-derived POR list CSV dump can be found."
@@ -40,9 +40,9 @@ else
 fi
 if [ "${SNPSHT_DATE}" != "" ]
 then
-	SNPSHT_DATE_HUMAN=`$DATE_TOOL -d ${SNPSHT_DATE}`
+	SNPSHT_DATE_HUMAN=$(${DATE_TOOL} -d ${SNPSHT_DATE})
 else
-	SNPSHT_DATE_HUMAN=`$DATE_TOOL --date='yesterday'`
+	SNPSHT_DATE_HUMAN=$(${DATE_TOOL} --date='yesterday')
 fi
 
 #
@@ -52,7 +52,7 @@ echo "bzip2 ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv"
 echo
 echo "On the local host:"
 echo "scp myuser@remote:~/dev/geo/opentraveldata/tools/{por_intorg_${TODAY_DATE}.csv.bz2,${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv.bz2} ."
-echo "bunzip2 -k por_intorg_${TODAY_DATE}.csv.bz2 && mv por_intorg_${TODAY_DATE}.csv dump_from_geonames.csv && mv por_intorg_${TODAY_DATE}.csv.bz2 ${ARCH_DIR}"
+echo "bunzip2 -k por_intorg_${TODAY_DATE}.csv.bz2 && mv por_intorg_${TODAY_DATE}.csv dump_from_geonames.csv && ln -s dump_from_geonames.csv por_intorg_${TODAY_DATE}.csv && rm -f por_intorg_${SNPSHT_DATE}.csv && mv por_intorg_${TODAY_DATE}.csv.bz2 ${ARCH_DIR}"
 echo "bunzip2 ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv.bz2"
 echo "patch -p0 --dry-run < ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv"
 echo "patch -p0 < ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv && mv ${POR_FILE_PFX}_${SNPSHT_DATE}.csv ${POR_FILE_PFX}_${TODAY_DATE}.csv && rm -f ${POR_FILE_PFX}_${SNPSHT_DATE}_${TODAY_DATE}.csv"
