9d0f7e58ebd2d0bf3823661d29432c431b0252bb
Steve-Z <31359112+Steve-Z@users.noreply.github.com>
2019-01-15 18:04:27 -0600

    scrape_optd_airlines_websites.py
    Python tool, dependent on requests and lxml packages, to scrape airline websites from Wikipedia page

---
+++ tools/scrape_optd_airlines_websites.py
@@ -0,0 +1,32 @@
+import requests 
+from lxml import html
+import csv
+
+new_rows = [["pk", "2char_code", "name", "website" ]]
+successes = 0
+failures = 0
+with open('optd_airline_best_known_so_far.csv', newline='') as csvfile:
+    rows = csv.reader(csvfile, delimiter='^')
+    for row in rows:
+        if row[0].startswith('air') and row[5] and row[7]:
+            try:
+                page = requests.get(row[12])
+                print('Connecting to:\n', row[12], '\n...')
+                if page.status_code == 200:
+                    print('Success\n')
+                tree = html.fromstring(page.content)
+                header = tree.xpath('//th[text()="Website"]')
+                site = header[0].xpath('..//a')
+                url = site[0].attrib['href']
+                successes += 1
+            except:
+                url = ""
+                print('Attempt failed:', row[7], '\n')
+                failures += 1
+            new_rows.append([row[0], row[5], row[7], url])
+
+print(successes, 'Successes,', failures, 'failures')                
+with open('optd_airlines_websites.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter='^')
+    writer.writerows(new_rows)
+
