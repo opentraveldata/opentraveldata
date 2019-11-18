a00512546d6cc4a46893482e2754ec5a685f8880
bat-cha <baptiste.chatrain@gmail.com>
2019-11-18 16:30:56 +0100

    idea: scrapping logos

---
+++ tools/scrape_optd_airlines_logos_from_websites.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import csv
+
+import requests
+
+successes = 0
+failures = 0
+with open('../opentraveldata/optd_airlines_websites_wkdt.csv', newline='') as csvfile:
+    rows = csv.reader(csvfile, delimiter='^')
+    for row in rows:
+        iata_code = row[1]
+        website = row[3]
+        if iata_code and website:
+            url = "https://logo.clearbit.com/"+website
+            try:
+                logo = requests.get(url)
+                print('Connecting to:\n', url, '\n...')
+                if logo.status_code == 200:
+                    print('Success\n')
+                    filename = '../opentraveldata/optd_airlines_websites_logos/' + iata_code+'.png'
+                    open(filename, 'wb').write(logo.content)
+                successes += 1
+            except:
+                print('Attempt failed:', url, '\n')
+                failures += 1
+
+
+print(successes, 'Successes,', failures, 'failures')
