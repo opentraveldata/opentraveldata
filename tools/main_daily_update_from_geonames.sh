497dc079f005e6c59e1ea4404282d50875a07057
Alex <alexprengere@gmail.com>
2022-01-24 14:51:20 +0100

    Integrate also airports and pageranks in daily updates

--- tools/main_daily_update_from_geonames.sh
+++ tools/main_daily_update_from_geonames.sh
@@ -8,6 +8,14 @@ echo "Processing ${TODAY} ..."
 ./extract_por_from_geonames.sh
 ./extract_por_from_geonames.sh --clean
 cp -f por_intorg_${TODAY}.csv dump_from_geonames.csv
+
+# For pageranks
+./make_optd_ref_pr_and_freq.py
+
+# For airlines
+./make_optd_airline_public.py
+
+# For POR
 ./make_optd_por_public.sh
 ./make_optd_por_public.sh --clean
 ./extract_por_unlc.sh
